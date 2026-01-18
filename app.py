63a894ed2b5c8c317f5981506a14d1ccb0c62aa2
Joshua Netzley Sr. <joshua.netzley.sr@gmail.com>
2026-01-18 17:31:40 -0500

    Implement web form to add new karts to database

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import sqlite3
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 
 app = Flask(__name__)
 
@@ -14,6 +14,20 @@ def index():
     karts = conn.execute('SELECT * FROM karts').fetchall()
     conn.close()
     return render_template('index.html', karts=karts)
+
+@app.route('/add', methods=('POST',))
+def add_kart():
+    name = request.form['name']
+    chassis = request.form['chassis']
+    engine = request.form['engine']
+    year = request.form['year']
+
+    conn = get_db_connection()
+    conn.execute('INSERT INTO karts (name, chassis_model, engine_type, year) VALUES (?, ?, ?, ?)',
+                 (name, chassis, engine, year))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
